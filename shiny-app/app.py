5eb3e31be461997b8c6d6c70bee1353761f96eb5
Olivér Zoltán Karsai <oliver.karsai@stud.uni-corvinus.hu>
2025-04-03 18:51:55 +0200

    Shiny app: updating word clouds with preprocessing

--- shiny-app/app.py
+++ shiny-app/app.py
@@ -6,27 +6,47 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import io
 import base64
+from collections import Counter
+import gensim
+import spacy
+from spacy.lang.hu.stop_words import STOP_WORDS
+from spacy.lang.hu import Hungarian
+nlp=Hungarian()
+
+hu = spacy.load('hu_core_news_lg')
+stopwords = hu.Defaults.stop_words
 
 all_tags = set(tag for tags in df["tags"] if isinstance(tags, list) for tag in tags)
 
-ui.page_opts(title="Word Cloud from JSON")
+ui.page_opts(title="Word Cloud for News Articles")
 
 with ui.sidebar():
     ui.input_select("topic", "Choose Topic", choices=list(all_tags), selected=list(all_tags)[0]),
     ui.input_slider("max_words", "Maximum Words:", min=10, max=200, value=100)
 
+# Preprocessing function
+def preprocess(word_list):
+    result = []
+    for token in word_list:
+        token=token.lemma_
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3 and token not in stopwords:
+            result.append(token)
+    return result
+
 def get_filtered_text(topic):
         filtered_texts = df[df["tags"].apply(lambda x: topic in x if isinstance(x, list) else False)]["article_text"].tolist()
-        return " ".join(filtered_texts)
-    
+        text = " ".join(filtered_texts)
+        return preprocess(hu(text))
+
 @render.ui
 def wordcloud():
     topic = input.topic()
     text = get_filtered_text(topic)
-    if not text.strip():
+    if not text:
         return "No words to display."
-        
-    wordcloud = WordCloud(width=1000, height=500, max_words=input.max_words(), background_color="white", colormap="viridis").generate(text)
+    
+    word_frequencies = Counter(text)
+    wordcloud = WordCloud(width=1000, height=500, max_words=input.max_words(), background_color="white", colormap="viridis").generate_from_frequencies(word_frequencies)
     fig, ax = plt.subplots(figsize=(10, 5), dpi=150)
     ax.imshow(wordcloud, interpolation="bilinear")
     ax.axis("off")
